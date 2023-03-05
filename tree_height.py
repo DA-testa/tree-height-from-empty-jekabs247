0d284e49eb9225e67526a2534d80e0049fd1d555
221RDC047 <93982525+jekabs247@users.noreply.github.com>
2023-03-05 17:31:58 +0000

    test9

--- tree_height.py
+++ tree_height.py
@@ -4,39 +4,46 @@ import sys
 import threading
 import numpy
 
+def find_root(n, parents):
+    # Find the root node
+    root = -1
+    for i in range(n):
+        if parents[i] == -1:
+            root = i
+            break
+    return root
 
 def compute_height(n, parents):
+    # Find the root node
+    root = find_root(n, parents)
 
+    # Create an array to store the heights of each node
     heights = numpy.zeros(n, dtype=numpy.int64)
+
+    # Calculate the height of each node
     max_height = 0
-
     for i in range(n):
-
-        current = i
+        if heights[i] != 0:
+            continue
         height = 0
-
-        while current != -1:
-
-            if heights[current] != 0:
-                height += heights[current]
+        node = i
+        while node != -1:
+            if heights[node] != 0:
+                height += heights[node]
                 break
-
             height += 1
-            current = parents[current]
-
+            node = parents[node]
         heights[i] = height
         max_height = max(max_height, height)
 
     return max_height
 
 def main():
-
     input_method = input()
 
     if input_method.lower() == 'i':
         n = int(input("Enter the number of nodes: "))
         parents = list(map(int, input("Enter the parent of each node: ").split()))
-
     else:
         n = int(input("Enter the number of nodes: "))
         parents = list(map(int, input("Enter the parent of each node: ").split()))
